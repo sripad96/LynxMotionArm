3009a29e638e7a97b9e25a0ef16e8e901551d05d
Alele Famous <famousalele@gmail.com>
2021-12-27 14:19:02 +0000

    Add files via upload
    Implement Inverse Kinematics

--- LynxFK.m
+++ LynxFK.m
@@ -58,12 +58,6 @@ l5 = 1;
 pt = [ xt yt zt ] ;
 
 % xt =cos(q1)*cos(q2)*l2 - l5*(cos((180*conj(q4))/pi)*(cos((180*conj(q1))/pi)*cos((180*conj(q2))/pi)*sin((180*conj(q3))/pi) + cos((180*conj(q1))/pi)*cos((180*conj(q3))/pi)*sin((180*conj(q2))/pi)) + sin((180*conj(q4))/pi)*(cos((180*conj(q1))/pi)*cos((180*conj(q2))/pi)*cos((180*conj(q3))/pi) - cos((180*conj(q1))/pi)*sin((180*conj(q2))/pi)*sin((180*conj(q3))/pi))) + cos((180*conj(q1))/pi)*cos((180*conj(q2))/pi)*cos((180*conj(q3))/pi)*conj(l3) - cos((180*conj(q1))/pi)*sin((180*conj(q2))/pi)*sin((180*conj(q3))/pi)*conj(l3)
- 
-
-% subs(xt(1, 1))
-% eval(subs(xt(1, 1)))
-
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Plot the trajectory of the end-effector
@@ -80,43 +74,108 @@ pt = [ xt yt zt ] ;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Workspace
-step = 30; 
+stepq1 = 30; 
+step = 60;
 points = 360/step;
 
 %% Plot the workspace of the robot
 figure (3)
 set(3,'position',[1243 190 560 420])
-
-% xwork = zeros(1024, 1024) ; % reserving space for the variables, because
-% ywork = zeros(1024, 1024) ; % otherwise they would be created later within a loop.
-% zwork = zeros(1024, 1024) ; 
 i = 1;
 xwork = zeros(points^2) ; % reserving space for the variables, because
 ywork = zeros(points^2) ; % otherwise they would be created later within a loop.
-zwork = zeros(points^2) ; 
+zwork = zeros(points^2) ;
+QVale = zeros(points^2);
 q5 = 0;
-for q1 = -90:180:90	% for q1
+
+for q1 = -90:stepq1:90	% for q1
     for q2 = 0:step:180   % for q2
         for q3 = -170:step:0  % for q3
             for q4 = 0:step:180 % for q4
                     xwork(i) =  eval(subs(xt));
                     ywork(i) =  eval(subs(yt));
                     zwork(i) =  eval(subs(zt));
+                    QVale(i) =  q2 + q3 + q4;
+                    if(xwork(i) < -0.78 & xwork(i) > -0.79)
+                          xwork(i)
+                          QVale(i)
+                          q1
+                          q2
+                          q3
+                          q4
+                    end
                     i = i+1;
-%                     plot(xwork,ywork, '.')
-%                     title('X-Y') ; xlabel('x (m)') ; ylabel('y (m)')
-%                     plot(ywork,zwork, 'rx')
-%                     title('Y-Z') ; ylabel('y (m)') ; zlabel('z (m)')
-%                     plot(xwork,zwork, '.')
-%                     title('X-Z') ; xlabel('x (m)') ; zlabel('z (m)')
-                    
-%                     i = i+1;
             end
         end
    end
 end
 
-plot3(xwork,ywork,zwork, 'rx')
+% xwork
+% ywork
+% zwork
+% QVale
+
+% plot3(xwork,ywork,zwork, 'rx')
+% hold on
+% title('Workspace') ; xlabel('x (m)') ; ylabel('y (m)') ; zlabel('z (m)') ;
+% axis equal
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Inverse Kinematics Section
+
+% xinvMat = [0 0.829228 1.43626 1.46914 0.787072 0]';
+% yinvMat = [-0.954885 -1.43626 -0.829228 0 0.454416 0.954885]';
+% zinvMat = [0.484332 1.05487 1.05487 1.21876 1.74277 0.484332]';
+% QinvMat = [110 190 190 10 -50 -110]';
+
+xinvMat = [-0.781237  -0.826955  -0.477442   0          0.462012   0.950612]';
+yinvMat = [ 0         -0.477442  -0.826955  -0.954885  -0.800229  -0.548836]';
+zinvMat = [0.530861    1.51567    1.51567    1.51567    1.56912    0.584313]';
+QinvMat = [130         70         70         70        -50        -110]';
+
+figure (4)
+set(4,'position',[1200 190 560 420])
+
+plot3(xinvMat, yinvMat, zinvMat, 'rx')
 hold on
 title('Workspace') ; xlabel('x (m)') ; ylabel('y (m)') ; zlabel('z (m)') ;
-axis equal
+
+%%
+%%Solve the Inverse Kinematics Model
+syms zinv xinv yinv Qinv;
+zPrime = zinv - (l3 * cosd(Qinv));
+rPrime = sqrt(xinv^2 + yinv^2) - (l3 * cosd(Qinv));
+gamma_a = (-1*zPrime) / sqrt(zPrime^2 + rPrime^2);
+gamma_b = rPrime / sqrt(zPrime^2 + rPrime^2);
+gamma = atan2d(gamma_a, gamma_b);
+Theta1 = atan2d(yinv, xinv);
+Theta2_a = gamma + acosd((-1*(rPrime^2 + zPrime^2 + l1^2 - l2^2))/(l1*2*sqrt(rPrime^2 + zPrime^2)));
+Theta2_b = gamma - acosd((-1*(rPrime^2 + zPrime^2 + l1^2 - l2^2))/(l1*2*sqrt(rPrime^2 + zPrime^2)));
+
+Theta3_a = atan2d((zPrime - (l1*sind(Theta2_a)))/l2, (rPrime - (l1*cosd(Theta2_a)))/l2) - Theta2_a;
+Theta3_b = atan2d((zPrime - (l1*sind(Theta2_b)))/l2, (rPrime - (l1*cosd(Theta2_b)))/l2) - Theta2_b;
+
+Theta4_a = Qinv - Theta2_a - Theta3_a;
+Theta4_b = Qinv - Theta2_b - Theta3_b;
+
+for i = 1:6
+    zinv = zinvMat(i);
+    yinv = yinvMat(i);
+    xinv = xinvMat(i);
+    Qinv = QinvMat(i);
+    eval(subs(gamma));
+    eval(subs(Theta1))
+    eval(subs(Theta2_a))
+    eval(subs(Theta3_a))
+    eval(subs(Theta4_a))
+end
+
+q1 = 180;
+q2 = 117.8254;
+q3 = -109.1271;
+q4 = 121.3018;
+
+xsolution = eval(subs(xt))
+ysolution = eval(subs(yt))
+zsolution = eval(subs(zt))
