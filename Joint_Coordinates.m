a3ef673dc53a30eb1dbce8791208db1d22b2f07e
Alele Famous <famousalele@gmail.com>
2021-12-31 00:09:36 +0000

    Update Joint_Coordinates.m

--- Joint_Coordinates.m
+++ Joint_Coordinates.m
@@ -1,21 +1,20 @@
-function J_Cor = Joint_Coordinates(q1, q2, q3, q4, l1, l2, l3, l5)
-
+function J_Cor = Joint_Coordinates(xinv, yinv, zinv, Qinv, l1, l2, l3, l5)
+    Lynx_IK = Inverse_Kinematics(xinv, yinv, zinv, Qinv, l1, l2, l3, l5);
+    q1 = Lynx_IK(2, 1);
+    q2 = Lynx_IK(2, 2);
+    q3 = Lynx_IK(2, 3);
+    q4 = Lynx_IK(2, 4);
+    l1 = 1; l2 = 1; l3 = 1; l5 = 1;
     J2x = (l2*cosd(q2))*cosd(q1);
     J2y = (l2*cosd(q2))*sind(q1);
     J2z = l1 + (l2*sind(q2));
 
-    J3x = J2x + ((l3*cosd(q3))*cosd(q1));
-    J3y = J2y + ((l3*cosd(q3))*sind(q1));
-    J3z = J2z + (l3*sind(q3));
-
-    J4x = J3x + ((l5*cosd(q4))*cosd(q1));
-    J4y = J3y + ((l5*cosd(q4))*sind(q1));
-    J4z = J3z + (l5*sind(q4));
-
-
-        
-    J_Cor = [0 0 J2x J3x J4x;
-             0 0 J2y J3y J4y; 
-             0 l1 J2z J3z J4z;]
+    J3x = J2x + ((l3*cosd(q3+q2))*cosd(q1));
+    J3y = J2y + ((l3*cosd(q3+q2))*sind(q1));
+    J3z = J2z + (l3*sind(q3+q2));
+      
+    J_Cor = [0 0 J2x J3x xinv;
+             0 0 J2y J3y yinv; 
+             0 l1 J2z J3z zinv;]
 
 end
