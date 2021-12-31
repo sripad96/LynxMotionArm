9f7b4429a3b17545ff307318ee12f1e019eaeb50
Alele Famous <famousalele@gmail.com>
2021-12-31 00:07:36 +0000

    Update LynxFK.m
    Implement task

--- LynxFK.m
+++ LynxFK.m
@@ -23,7 +23,7 @@ l2 = 1;
 l3 = 1;
 l4 = 0;
 l5 = 1;
-%% Trigonometric angles
+%% Joint angles
 syms q1 q2 q3 q4 q5;
 
 
@@ -31,7 +31,8 @@ syms q1 q2 q3 q4 q5;
 % T_ij = eye(4);
 
 %% Tip position
-% These equations are derived from the Forward Kinematic model of the 2DOF
+% These equations are derived from the Forward Kinematic model of the lynx
+% robot
 % robot
 
 a = [0 l2 l3 0 0]';
@@ -50,32 +51,20 @@ T_ij5 = Transformation(a(5), alpha(5), d(5), theta(5));
 
 T_ij  = T_ij1 * T_ij2 * T_ij3 * T_ij4 * T_ij5;
 
-xt = T_ij(1, 4)
+% xt = T_ij(1, 4)
+% yt = T_ij(2, 4)
+% zt = T_ij(3, 4)
 
-yt = T_ij(2, 4)
-
-zt = T_ij(3, 4)
+xt = cosd(q1)*((l5*cosd(q2+q3+q4))+(l3*cosd(q2+q3))+(l2*cosd(q2)))
+yt = sind(q1)*((l5*cosd(q2+q3+q4))+(l3*cosd(q2+q3))+(l2*cosd(q2)))
+zt = (l5*sind(q2+q3+q4))+(l3*sind(q2+q3))+(l2*sind(q2))+l1
 
 % xt =cos(q1)*cos(q2)*l2 - l5*(cos((180*conj(q4))/pi)*(cos((180*conj(q1))/pi)*cos((180*conj(q2))/pi)*sin((180*conj(q3))/pi) + cos((180*conj(q1))/pi)*cos((180*conj(q3))/pi)*sin((180*conj(q2))/pi)) + sin((180*conj(q4))/pi)*(cos((180*conj(q1))/pi)*cos((180*conj(q2))/pi)*cos((180*conj(q3))/pi) - cos((180*conj(q1))/pi)*sin((180*conj(q2))/pi)*sin((180*conj(q3))/pi))) + cos((180*conj(q1))/pi)*cos((180*conj(q2))/pi)*cos((180*conj(q3))/pi)*conj(l3) - cos((180*conj(q1))/pi)*sin((180*conj(q2))/pi)*sin((180*conj(q3))/pi)*conj(l3)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Plot the trajectory of the end-effector
-%figure (1)
-%set(1,'position',[680 558 560 420])
-
-%plot3(pt(1,1),pt(1,2),0,'rx')       % plot the first position of the robot's end effector
-%hold on
-%plot(pt(2:4,1),pt(2:4,2),'x')       % plot the 3 following positions of the robot's end effector
-%title('Tip Trajectory') ; xlabel('x (m)') ; ylabel('y (m)') ;
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Workspace
-stepq1 = 30; 
-step = 60;
-points = 360/step;
+stepq1 = 10; 
+step = 10;
+points = 180/step;
 
 %% Plot the workspace of the robot
 figure (3)
@@ -87,35 +76,33 @@ zwork = zeros(points^2) ;
 QVale = zeros(points^2);
 q5 = 0;
 
-for q1 = -90 :stepq1:90	% for q1
-    for q2 = 0:step:180   % for q2
-        for q3 = 0:step:180 % for q3
-            for q4 = 0:step:180 % for q4
-                    xwork(i) =  eval(subs(xt));
-                    ywork(i) =  eval(subs(yt));
-                    zwork(i) =  eval(subs(zt));
-                    QVale(i) =  q2 + q3 + q4;
-%                     if(zwork(i) < 1.74 & zwork(i) > 1.73)
-%                           xwork(i)
-%                           ywork(i)
-%                           zwork(i)
-%                           QVale(i)
-%                           q1
-%                           q2
-%                           q3
-%                           q4
-%                     end
-                    i = i+1;
-            end
-        end
-   end
-end
-
-% xwork
-% ywork
-% zwork
-% QVale
-
+% for q1 = -90 :stepq1:90	% for q1
+%     for q2 = 0:step:135   % for q2
+%         for q3 = -145:step:0 % for q3
+%             for q4 = -90:step:90 % for q4
+%                     xwork(i) =  eval(subs(xt));
+%                     ywork(i) =  eval(subs(yt));
+%                     zwork(i) =  eval(subs(zt));
+%                     QVale(i) =  q2 + q3 + q4;
+% %                     if(zwork(i) < 1.74 & zwork(i) > 1.73)
+% %                           xwork(i)
+% %                           ywork(i)
+% %                           zwork(i)
+% %                           QVale(i)
+% %                           q1
+% %                           q2
+% %                           q3
+% %                           q4
+% %                     end
+%                     i = i+1;
+%             end
+%         end
+%    end
+% end
+                          xwork
+                          ywork
+                          zwork
+                          QVale
 plot3(xwork,ywork,zwork, 'rx')
 hold on
 title('Workspace') ; xlabel('x (m)') ; ylabel('y (m)') ; zlabel('z (m)') ;
@@ -123,71 +110,14 @@ axis equal
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%Inverse Kinematics Section
-
-xinvMat = [-0.781237  -0.826955  -0.477442   0          0.462012   0.950612]';
-yinvMat = [ 0         -0.477442  -0.826955  -0.954885  -0.800229  -0.548836]';
-zinvMat = [0.530861    1.51567    1.51567    1.51567    1.56912    0.584313]';
-QinvMat = [130         70         70         70        -50        -110]';
-
-figure (4)
-set(4,'position',[1200 190 560 420])
-
-plot3(xinvMat, yinvMat, zinvMat, 'rx')
-hold on
-title('Workspace') ; xlabel('x (m)') ; ylabel('y (m)') ; zlabel('z (m)') ;
-
-%%
-%%Solve the Inverse Kinematics Model
-syms xinv yinv zinv Qinv;
-Theta1 = atan2d(yinv, xinv)
-
-R = l5*cosd(Qinv);
-xPrime = xinv - (R*cosd(Theta1));
-yPrime = yinv - (R*sind(Theta1));
-zPrime = zinv - (l5 * sind(Qinv));
-
-rPrime = sqrt(xinv^2 + yinv^2) - R
-N = sqrt((zPrime - l1)^2 + rPrime^2);
-gamma = atan2d((zPrime - l1), rPrime);
-Theta2_a = gamma - acosd((N^2 + l2^2 - l3^2) / (2*l2*N))
-Theta2_b = gamma + acosd((N^2 + l2^2 - l3^2) / (2*l2*N))
-
-Theta3_a = acosd((N^2 - l2^2 - l3^2)/(2*l2*l3))
-Theta3_b = -1*acosd((N^2 - l2^2 - l3^2)/(2*l2*l3))
-Theta4_a = Qinv - Theta2_a - Theta3_a;
-Theta4_b = Qinv - Theta2_b - Theta3_b;
-
-
-
-    i = 2;
-%     zinv = zinvMat(i);
-    zinv = 4;
-%     yinv = yinvMat(i);
-    yinv = 0;
-%     xinv = xinvMat(i)
-    xinv = 0;
-%     Qinv = QinvMat(i);
-    Qinv = 90;
-    disp("INVERSE KINEMATICS")
-    q1 = eval(subs(Theta1))
-    q2 = eval(subs(Theta2_a))
-    q3 = eval(subs(Theta3_a))
-    q4 = eval(subs(Theta4_a))
-    disp("Solution B")
-    q1 = eval(subs(Theta1))
-    q2 = eval(subs(Theta2_b))
-    q3 = eval(subs(Theta3_b))
-    q4 = eval(subs(Theta4_b))
-    
-
+%% Inverse Kinematics Section
 
 q1 = 0;    
 q2 = 0;
 % q2 = 180;
-q3 = 0;
+q3 = 90;
 % q3 = -170;
-q4 = 0;
+q4 = -150;
 % q4 = 120;
 q5 = 0;
 
@@ -197,15 +127,59 @@ zsolution = eval(subs(zt))
 
 
    
-%% Plot the robotic arm, in 6 different positions
+%%Plot the robotic arm, in at least different positions
  %%SOLVING FOR JOINT COORDINATES
 figure (2) 
 
 
+Task = [1.04168 -1.80423  0.090961  5;
+        2.01691 -1.16446  1.48369   65;
+        1.82893  0        2.34971   65;
+        2.00515  1.15767  2.52676   5;
+        0.989942 1.171463 0.624255 -55;
+        0        2.08335  0.090961  5;]
 
-J0 = zeros(6,3);
-J1 = zeros(6,3);
-J2 = zeros(6,3);
-J3 = zeros(6,3);
-J4 = zeros(6,3);
-J5 = zeros(6,3);
+for i = 1:6
+    J_Cor = Joint_Coordinates(Task(i, 1), Task(i, 2), Task(i, 3), Task(i, 4), l1, l2, l3, l5);
+    xx = [ J_Cor(1, 1); J_Cor(1, 2); J_Cor(1, 3); J_Cor(1, 4); J_Cor(1, 5)]; 
+    yy = [ J_Cor(2, 1); J_Cor(2, 2); J_Cor(2, 3); J_Cor(2, 4); J_Cor(2, 5)];
+    zz = [ J_Cor(3, 1); J_Cor(3, 2); J_Cor(3, 3); J_Cor(3, 4); J_Cor(3, 5)]; 
+    plot3(xx, yy, zz, 'ko-','Linewidth', 2)
+    axis equal
+    hold on
+    xlabel('x (units)') ; ylabel('y (units)'); zlabel('z (units)');
+    T = num2str(xx(1,1), 5) + ", " + num2str(yy(1,1), 5) + ", " + num2str(zz(1,1), 5);
+    text(xx(1,1) + 0.02, yy(1,1) + 0.02, zz(1,1) + 0.02, T);
+    axis([-4 4 -4 4]);
+    pause(1.5)
+    hold off
+    pause(0.1)
+end
+
+
+% text(pt(4,1),pt(4,2),'x'); 
+% text(pt(4,1) + 0.002,pt(4,2) + 0.002,'ptEnd');
+
+disp("Display Inverse Kinematics Plot")
+J_Cor = Joint_Coordinates(Task(2, 1), Task(2, 2), Task(2, 3), Task(2, 4), l1, l2, l3, l5);
+    xx = [ J_Cor(1, 1); J_Cor(1, 2); J_Cor(1, 3); J_Cor(1, 4); J_Cor(1, 5)] 
+    yy = [ J_Cor(2, 1); J_Cor(2, 2); J_Cor(2, 3); J_Cor(2, 4); J_Cor(2, 5)] 
+    zz = [ J_Cor(3, 1); J_Cor(3, 2); J_Cor(3, 3); J_Cor(3, 4); J_Cor(3, 5)] 
+    plot3(xx, yy, zz, 'ko-','Linewidth', 2)
+    hold on
+    xlabel('x (units)') ; ylabel('y (units)'); zlabel('z (units)');
+    T = num2str(xx(1,1), 5) + ", " + num2str(yy(1,1), 5) + ", " + num2str(zz(1,1), 5);
+    text(xx(1,1) + 0.02, yy(1,1) + 0.02, zz(1,1) + 0.02, T);
+
+    T = num2str(xx(2,1), 5) + ", " + num2str(yy(2,1), 5) + ", " + num2str(zz(2,1), 5);
+    text(xx(2,1) + 0.02, yy(2,1) + 0.02, zz(2,1) + 0.02, T);
+
+    T = num2str(xx(3,1), 5) + ", " + num2str(yy(3,1), 5) + ", " + num2str(zz(3,1), 5);
+    text(xx(3,1) + 0.02, yy(3,1) + 0.02, zz(3,1) + 0.02, T);
+
+    T = num2str(xx(4,1), 5) + ", " + num2str(yy(4,1), 5) + ", " + num2str(zz(4,1), 5);
+    text(xx(4,1) + 0.02, yy(4,1) + 0.02, zz(4,1) + 0.02, T);
+
+    T = num2str(xx(5,1), 5) + ", " + num2str(yy(5,1), 5) + ", " + num2str(zz(5,1), 5);
+    text(xx(5,1) + 0.02, yy(5,1) + 0.02, zz(5,1) + 0.02, T);
+%     text(xx(1) + 0.02, yy(1,1) + 0.02, zz(1,1) + 0.02, '');
