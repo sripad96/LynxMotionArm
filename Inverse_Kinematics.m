b73d1132caf22613a496cbdf364b629d3f144835
Alele Famous <famousalele@gmail.com>
2021-12-31 00:08:41 +0000

    Update IK function

--- Inverse_Kinematics.m
+++ Inverse_Kinematics.m
@@ -1,11 +1,11 @@
-function Lynx_IK = Inverse_Kinematics(xinv, yinv, zinv, Qinv)
+function Lynx_IK = Inverse_Kinematics(xinv, yinv, zinv, Qinv, l1, l2, l3, l5)
     Theta1 = atan2d(yinv, xinv);
     R = l5*cosd(Qinv);
 %     xPrime = xinv - (R*cosd(Theta1));
 %     yPrime = yinv - (R*sind(Theta1));
     zPrime = zinv - (l5 * sind(Qinv));
 
-    rPrime = sqrt(xinv^2 + yinv^2) - R
+    rPrime = sqrt(xinv^2 + yinv^2) - R;
     N = sqrt((zPrime - l1)^2 + rPrime^2);
     gamma = atan2d((zPrime - l1), rPrime);
     Theta2_a = gamma - acosd((N^2 + l2^2 - l3^2) / (2*l2*N));
